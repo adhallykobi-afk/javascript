9349e8d80e58743b4dad848c3e723643dd8ab0f6
pedrotiburskilima-arch <adhallykobi@gmail.com>
2026-09-11 09:20:36 -0300

    Update tipos- de-dados-boolean.js

--- tipos- de-dados-boolean.js
+++ tipos- de-dados-boolean.js
@@ -1,2 +1,6 @@
 let aprovado = true; // Boolleano verdadeiro
-let reprovado
+let reprovado = false // Booleano falso
+console.log("Aprovado:", aprovado);
+console.log("Reprovado:", reprovado);
+console.log("Tipo de aprovado:", typeof aprovado);
+console.log("Tipo de reprovado:", typeof reprovado);
